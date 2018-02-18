051df8222749ed3e03fd98ef8a795e8fbafae18e
sumtype <james@sumtype.tech>
2018-02-18 16:20:54 -0500

    Updated test case.

--- algorithms/es/countingSort.js
+++ algorithms/es/countingSort.js
@@ -6,14 +6,10 @@
 
 const countingSort = (array = []) => {
   if (array.length === 0) return []
-  let min = 100000000000000000000000000000
-  let max = 0
+  let min = Math.min(...array)
+  let max = Math.max(...array)
   let val = 0
   let counts = null
-  for (let i = 0; i < array.length; i++) {
-    if (min > array[i]) min = array[i]
-    else if (max < array[i]) max = array[i]
-  }
   for (let i = -1; i < array.length; i++) i === -1 ? counts = new Array(max - min + 1).fill(0) : counts[array[i] - min]++
   for (let i = 0; i < counts.length; i++) for (let j = 0; j < counts[i]; j++) array[val++] = i + min
   return array
