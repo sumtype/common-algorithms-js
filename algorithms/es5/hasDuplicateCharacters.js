13f2a295fe1b182a3d0d96075bcd8134d51a88c5
sumtype <james@sumtype.tech>
2018-02-18 15:31:16 -0500

    Updated Coveralls test reporting.

--- algorithms/es5/hasDuplicateCharacters.js
+++ algorithms/es5/hasDuplicateCharacters.js
@@ -18,8 +18,7 @@
   })
   var hasDuplicateCharacters = function hasDuplicateCharacters () {
     var s = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ''
-    var characters = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : new Set(s)
-    return characters.size !== s.length
+    return new Set(s).size !== s.length
   }
 
   exports.default = hasDuplicateCharacters
